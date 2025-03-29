510b307201a2f9d7e972d43a84e7d562a9faea2c
tqkblx <il.mystafa@gmail.com>
2025-03-29 13:51:00 +0400

    Refactor

--- main.py
+++ main.py
@@ -1,46 +1,116 @@
 import numpy as np
 from matplotlib import pyplot
 
-def main():
-    image_set_count = 3
-    image_count = 5
-    image_x_size = 24
-    image_y_size = 24
-    solution_count = 1000
+class Fitter:
+    IMAGE_SET_COUNT = 3
+    IMAGE_COUNT = 5
+    IMAGE_X_SIZE = 24
+    IMAGE_Y_SIZE = 24
 
-    images = np.zeros((image_set_count * image_count,
-                       image_x_size, image_y_size))
-    for image_index in range(image_set_count * image_count):
-        image_file_name = f"{image_index}.bin"
-        with open(image_file_name, 'r') as image_file:
-            bit_string = image_file.read()
-        bit_string = bit_string.replace("\n", "")
-        for i in range(len(bit_string)):
-            images[image_index][i // image_x_size \
-                ][i % image_y_size] = \
-                int(bit_string[i])
+    SOLUTION_COUNT = 1000
+    PATTERN_COUNT = 7
+    PATTERN_X_SIZE = 3
+    PATTERN_Y_SIZE = 3
+    ITERATION_COUNT = 100
 
-    for image_set_index in range(image_set_count):
+    def __init__(self):
+        pass
+
+
+    def load_images(self):
+        images = np.zeros((Fitter.IMAGE_SET_COUNT * Fitter.IMAGE_COUNT,
+                           Fitter.IMAGE_X_SIZE, Fitter.IMAGE_Y_SIZE))
+        for image_index in range(Fitter.IMAGE_SET_COUNT * Fitter.IMAGE_COUNT):
+            image_file_name = f"{image_index}.bin"
+            with open(image_file_name, 'r') as image_file:
+                bit_string = image_file.read()
+            bit_string = bit_string.replace("\n", "")
+            for i in range(len(bit_string)):
+                images[image_index][i // Fitter.IMAGE_X_SIZE \
+                    ][i % Fitter.IMAGE_Y_SIZE] = \
+                    int(bit_string[i])
+
+        return images
+
+    def place_most_similar_pattern(self, solution, 
+                                   approximate_image,
+                                   current_image,
+                                   x_index,
+                                   y_index):
+        raise NotImplementedError
+
+    
+    def construct_approximate_image(self, image_set_index,
+                                    images, solution,
+                                    image_index): 
+        approximate_image = np.zeros(
+            (Fitter.IMAGE_X_SIZE, Fitter.IMAGE_Y_SIZE))
+        current_image_index = \
+            Fitter.IMAGE_COUNT * image_set_index + \
+            image_index
+        current_image = \
+            images[current_image_index]
+
+        for x_index in range(
+            0, Fitter.IMAGE_X_SIZE, Fitter.PATTERN_X_SIZE):
+            for y_index in range(
+                0, Fitter.IMAGE_Y_SIZE,
+                Fitter.PATTERN_Y_SIZE):
+                self.place_most_similar_pattern(
+                    solution, approximate_image, 
+                    current_image, x_index, y_index 
+                )
+
+        return approximate_image
+
+
+    def compute_loss(self, image_set_index, images,
+                     solution):
+        loss = 0
+
+        for image_index in range(Fitter.IMAGE_COUNT):
+            approximate_image = \
+                self.construct_approximate_image(
+                    image_set_index, images,
+                    solution, image_index
+                )
+        
+        return loss
+
+        
+    def compute_losses(self, image_set_index,
+                       images, solutions):
+        losses = np.zeros((Fitter.SOLUTION_COUNT,))
+        
+        for solution_index in range(Fitter.SOLUTION_COUNT):
+            losses[solution_index] = self.compute_loss(
+                image_set_index, images,
+                solutions[solution_index])
+
+        return losses
+
+
+    def fit_to_image_set(self, image_set_index, images):
         print(f"Image set: {image_set_index}")
 
         solutions = (np.random.rand(
-            solution_count, 7, 3, 3) > 0.5).astype(int)
+            Fitter.SOLUTION_COUNT, Fitter.PATTERN_COUNT,
+            Fitter.PATTERN_X_SIZE, Fitter.PATTERN_Y_SIZE) > 0.5
+            ).astype(int)
 
-        losses = np.zeros((solution_count,))
+        for current_iteration in range(Fitter.ITERATION_COUNT):
+            losses = self.compute_losses(image_set_index, images, solutions)
 
-        iteration_count = 100
 
-        for current_iteration in range(iteration_count):
-            for solution_index in range(solution_count):
-                losses[solution_index] = 0 
-                for image_index in range(image_count):
-                    approximate_image = np.zeros(
-                        (image_x_size, image_y_size))
-                    current_image_index = \
-                        image_count * image_set_index + \
-                        image_index
-                    current_image = \
-                        images[current_image_index]
+    def run(self):
+        images = self.load_images()
+
+        for image_set_index in range(Fitter.IMAGE_SET_COUNT):
+            self.fit_to_image_set(image_set_index, images)
+
+def main():
+    Fitter().run()
+
 
 if __name__ == "__main__":
     main()
